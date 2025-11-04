97aee36902add6b605f7eece0902c20ca3e0fbf5
Bekir Gülestan <gulestanbekir@gmail.com>
2025-11-04 16:44:22 +0100

    Implements authentication with Better Auth
    Sets up user authentication using the Better Auth library.
    Configures Prisma adapter for database interaction.
    Enables email/password and Google social login.
    Adds a 'role' field to the user model.
    Adds local env file to git ignore

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,31 +1,33 @@
-import { betterAuth } from "better-auth";
+import {betterAuth} from "better-auth";
 
-import { prismaAdapter } from "better-auth/adapters/prisma";
+import {prismaAdapter} from "better-auth/adapters/prisma";
 import prisma from "./prisma";
 
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
-        debugLogs:{
-            create:true
+        debugLogs: {
+            create: true
         }
     }),
-    emailAndPassword:{
+    emailAndPassword: {
         enabled: true,
     },
-    socialProviders:{
-        google:{
+    socialProviders: {
+        google: {
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }
     },
-    user:{
-        additionalFields:{
-            role:{
+    user: {
+        additionalFields: {
+            role: {
                 type: "string",
                 defaultValue: "user",
             }
         }
-    }
+    },
+
+    trustedOrigins: ["http://localhost:3000", process.env.BETTER_AUTH_URL!]
 });
