bb4d424b5646d79165b8577cd2cf84323ee79263
Bekir Gülestan <gulestanbekir@gmail.com>
2025-11-25 21:59:34 +0100

    Disables rewrites in next config
    Disables the rewrites configuration in the Next.js configuration file.
    
    This change moves the PostHog request forwarding logic to a proxy middleware, offering more flexibility and control over the request handling process.

--- next.config.ts
+++ next.config.ts
@@ -4,20 +4,22 @@ const nextConfig: NextConfig = {
     /* config options here */
     // This is required to support PostHog trailing slash API requests
     skipTrailingSlashRedirect: true,
-    async rewrites() {
-        return [
-            {
-                // "genuine" is a nickname for PostHog's relay server
-                // because we are genuine for user experience monitoring
-                source: "/genuine/:path*",
-                destination: process.env.NEXT_PUBLIC_POSTHOG_HOST + "/:path*",
-            },
-            {
-                source: "/genuine/static/:path*",
-                destination: "https://eu-assets.i.posthog.com/static/:path*",
-            },
-        ];
-    },
+    // [DISABLED] Use the proxy middleware instead of rewrites to forward PostHog requests
+    // [DISABLED] See src/app/proxy.ts for the implementation.
+    // async rewrites() {
+    //     return [
+    //         {
+    //             // "genuine" is a nickname for PostHog's relay server
+    //             // because we are genuine for user experience monitoring
+    //             source: "/genuine/:path*",
+    //             destination: process.env.NEXT_PUBLIC_POSTHOG_HOST + "/:path*",
+    //         },
+    //         {
+    //             source: "/genuine/static/:path*",
+    //             destination: "https://eu-assets.i.posthog.com/static/:path*",
+    //         },
+    //     ];
+    // },
 };
 
 export default nextConfig;
