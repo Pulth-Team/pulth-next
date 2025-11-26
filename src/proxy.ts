a3007a19763db6ad11a7105862cf09603f57e4e7
Bekir Gülestan <gulestanbekir@gmail.com>
2025-11-26 02:13:09 +0100

    move proxy to correct place

---
+++ src/proxy.ts
@@ -0,0 +1,47 @@
+import {NextResponse, userAgent} from 'next/server'
+import type {NextRequest} from 'next/server'
+
+
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+
+    const ua = userAgent(request);
+
+    if (ua.isBot) {
+        console.log("BOT DETECTED, SKIP");
+        return NextResponse.next();
+    }
+
+    const url = request.nextUrl.clone();
+
+    const isStatic = url.pathname.startsWith("/genuine/static/");
+    const targetHost = isStatic
+        ? "eu-assets.i.posthog.com"
+        : "eu.i.posthog.com";
+
+    // Remove /genuine prefix
+    const originalPath = url.pathname;
+    url.pathname = originalPath.replace(/^\/genuine/, "");
+
+    url.hostname = targetHost;
+    url.protocol = "https";
+
+    const finalUrl = url.toString();
+
+    // console.log("PROXY →", originalPath, "→", finalUrl);
+
+    const res = NextResponse.next();
+    res.headers.set("x-middleware-rewrite", finalUrl);
+    res.headers.set("host", targetHost);
+
+    return res;
+
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: ['/genuine/:path*'],
+}
