5dc5ff6ef28f42034910643ce17bf1db1f58aa5a
Bekir Gülestan <gulestanbekir@gmail.com>
2025-11-28 06:24:25 +0100

    add genuine route to disallow list

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,12 @@
-import type { MetadataRoute } from 'next'
+import type {MetadataRoute} from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/_next/','/api/'],
-    },
-    sitemap: 'https://www.pulth.com/sitemap.xml',
-  }
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: ['/_next/', '/api/', '/genuine/'],
+        },
+        sitemap: 'https://www.pulth.com/sitemap.xml',
+    }
 }
