69a005943af173c19adadc3d9fe824e22305339a
Bekir Gülestan <gulestanbekir@gmail.com>
2026-04-05 11:21:47 +0200

    generator fix for prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,19 @@
-import {PrismaClient} from '@/generated/prisma-client'
-import {PrismaNeon} from '@prisma/adapter-neon';
-import {neonConfig} from '@neondatabase/serverless';
-import ws from 'ws';
+import { PrismaClient } from "@/generated/prisma";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
 
 neonConfig.webSocketConstructor = ws;
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
+  prisma: PrismaClient;
+};
 
 const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaNeon({connectionString});
-const prisma = globalForPrisma.prisma || new PrismaClient({adapter});
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const adapter = new PrismaNeon({ connectionString });
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export default prisma;
-
 
 //
 // const prisma = globalForPrisma.prisma || new PrismaClient()
